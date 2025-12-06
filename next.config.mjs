e8944db3f4f8bc8fc9720f4ed1c09e49c2d4e71d
Sam Yaza <vvatcher6922@gmail.com>
2025-12-06 15:05:30 -0600

    Forcing webpack build at the package level instead of VErcel command

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable Turbopack (must be an object, not a boolean)
-  turbopack: {},
-
-  webpack: (config) => {
-    // Required for @napi-rs/canvas to work on server bundles
-    config.externals.push({
-      "@napi-rs/canvas": "commonjs @napi-rs/canvas",
-    });
-
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@napi-rs/canvas': 'commonjs @napi-rs/canvas'
+      });
+    }
     return config;
-  },
+  }
 };
 
 export default nextConfig;
